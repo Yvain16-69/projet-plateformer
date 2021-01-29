50fedb466c1a9d4f9e7db3ab031453d9a14e3268
Corentin1998 <corentin.hbb@gmail.com>
2021-01-29 22:31:08 +0100

    changements 29.01

--- src/scenes/TableauZoo.js
+++ src/scenes/TableauZoo.js
@@ -3,10 +3,11 @@ class TableauZoo extends Tableau{
     preload() {
         super.preload();
         this.load.image('star', 'assets/star.png');
-        this.load.image('monster-violet', 'assets/monster-violet.png');
+        this.load.image('goomba', 'assets/goomba.png');
         this.load.image('bowser', 'assets/bowser.png');
         this.load.image('monster-fly', 'assets/monster-fly.png');
         this.load.image('plante-pirahna', 'assets/plante-pirahna.png');
+        this.load.image('thwomp', 'assets/thwomp.png');
 
     }
     create() {
@@ -22,21 +23,21 @@ class TableauZoo extends Tableau{
             child.setBounce(1);
             child.setGravity(1);
             child.setCollideWorldBounds(true);
-            child.setVelocity( 0,Phaser.Math.Between(-100, 100));
+            child.setVelocity(0,Phaser.Math.Between(-100, 100));
             child.setMaxVelocity(0,500);
         });
         this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
 
         //notre monstre
-        this.monstre=this.physics.add.sprite(300,this.sys.canvas.height-70,"monster-violet");
+        this.monstre=this.physics.add.sprite(850,this.sys.canvas.height-70,"goomba");
         this.monstre.setOrigin(0,0);
         this.monstre.setDisplaySize(64,64);
         this.monstre.setCollideWorldBounds(true);
         this.monstre.setBounce(1);
-        this.monstre.setVelocityX(20);
+        this.monstre.setVelocityX(80);
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
 
-        this.monstre=this.physics.add.sprite(150,this.sys.canvas.height-70,"bowser");
+        this.monstre=this.physics.add.sprite(550,this.sys.canvas.height-70,"bowser");
         this.monstre.setOrigin(0,0);
         this.monstre.setDisplaySize(80, 80);
         this.monstre.setCollideWorldBounds(true);
@@ -44,13 +45,32 @@ class TableauZoo extends Tableau{
         this.monstre.setVelocityX(50);
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
 
-        this.monstre=this.physics.add.sprite(550,this.sys.canvas.height-70,"plante-pirahna");
+        this.monstre=this.physics.add.sprite(220,this.sys.canvas.height-70,"plante-pirahna");
         this.monstre.setOrigin(0,0);
         this.monstre.setDisplaySize(48, 90);
         this.monstre.setCollideWorldBounds(true);
         this.monstre.setBounce(0);
         this.monstre.setVelocityX(0);
         this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+
+        this.monstre=this.physics.add.sprite(270,this.sys.canvas.height-70,"thwomp");
+        this.monstre.setOrigin(3,7);
+        this.monstre.setDisplaySize(64, 80);
+        this.monstre.setCollideWorldBounds(false);
+        this.monstre.setBounce(0);
+        this.monstre.setVelocityX(0);
+        this.monstre.setVelocityY(-100);
+        this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+
+        this.monstre=this.physics.add.sprite(320,this.sys.canvas.height-70,"thwomp");
+        this.monstre.setOrigin(3,7);
+        this.monstre.setDisplaySize(64, 80);
+        this.monstre.setCollideWorldBounds(false);
+        this.monstre.setBounce(0);
+        this.monstre.setVelocityX(0);
+        this.monstre.setVelocityY(50);
+        this.physics.add.overlap(this.player, this.monstre, this.hitSpike, null, this);
+
     
         new MonsterFly(this,400,100);
     }
