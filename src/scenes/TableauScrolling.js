903e079fc994f8f5f286047795ea5875a7194302
Corentin1998 <corentin.hbb@gmail.com>
2021-02-24 18:22:01 +0100

    changements

--- src/scenes/TableauScrolling.js
+++ src/scenes/TableauScrolling.js
@@ -4,9 +4,9 @@ class TableauScrolling extends Tableau{
         super.preload();
         this.load.image('star', 'assets/star.png');
         this.load.image('ground', 'assets/platform.png');
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('sky-2', 'assets/sky-2.png');
-        this.load.image('sky-3', 'assets/sky-3.png')
+        this.load.image('ciel', 'assets/ciel.png');
+        this.load.image('premier-plan', 'assets/premier-plan.png');
+        this.load.image('deuxieme-plan', 'assets/deuxieme-plan.png')
         this.load.image('platform-vg', 'assets/platform-vg.png');
         this.load.image('platform-mg', 'assets/platform-mg.png');
         this.load.image('platform-vd', 'assets/platform-vd.png');
@@ -33,14 +33,14 @@ class TableauScrolling extends Tableau{
 
         // placement des pateformes
         this.platforms = this.physics.add.staticGroup();
-        this.platforms.create(224, 560, 'ground').setScale(1).refreshBody();
-        this.platforms.create(672, 560, 'ground').setScale(1).refreshBody();
-        this.platforms.create(1008, 480, 'platform-vg').setScale(1).refreshBody();
-        this.platforms.create(1008, 560, 'platform-mg').setScale(1).refreshBody();
-        this.platforms.create(1232, 480, 'platform-vd').setScale(1).refreshBody();
-        this.platforms.create(1232, 560, 'platform-md').setScale(1).refreshBody();
-        this.platforms.create(1568, 560, 'ground').setScale(1).refreshBody();
-        this.platforms.create(1904, 560, 'platform-vd').setScale(1).refreshBody();
+        this.platforms.create(224, 580, 'ground').setScale(1).refreshBody();
+        this.platforms.create(672, 580, 'ground').setScale(1).refreshBody();
+        this.platforms.create(1008, 500, 'platform-vg').setScale(1).refreshBody();
+        this.platforms.create(1008, 580, 'platform-mg').setScale(1).refreshBody();
+        this.platforms.create(1232, 500, 'platform-vd').setScale(1).refreshBody();
+        this.platforms.create(1232, 580, 'platform-md').setScale(1).refreshBody();
+        this.platforms.create(1568, 580, 'ground').setScale(1).refreshBody();
+        this.platforms.create(1904, 580, 'platform-vd').setScale(1).refreshBody();
 
         //étoiles
         //this.stars.create(300,0,"star").setCollideWorldBounds(true).setBounce(0.4);
@@ -50,39 +50,39 @@ class TableauScrolling extends Tableau{
         this.physics.add.collider(this.platforms, this.stars);
 
         //on change de ciel, on fait une tileSprite ce qui permet d'avoir une image qui se répètE
-        this.sky=this.add.tileSprite(
+        this.ciel=this.add.tileSprite(
             0,
             21,
             this.sys.canvas.width,
             this.sys.canvas.height,
-            'sky'
+            'ciel'
             );
-        this.sky.setOrigin(0,0);
-        this.sky.setScrollFactor(0);//fait en sorte que le ciel ne suive pas la caméra
+        this.ciel.setOrigin(0,0);
+        this.ciel.setScrollFactor(0);//fait en sorte que le ciel ne suive pas la caméra
     
         //on ajoute une deuxième couche de ciel
-            this.sky2=this.add.tileSprite(
+        this.deuxiemeplan=this.add.tileSprite(
             0,
             0,
             this.sys.canvas.width,
             this.sys.canvas.height,
-            'sky-2'
+            'deuxieme-plan'
             );
-        this.sky2.setScrollFactor(0);
-        this.sky2.setOrigin(0,0);
-        this.sky2.alpha=1;
-        this.sky2.tileScaleX=this.sky.tileScaleY=0.8;
+        this.deuxiemeplan.setScrollFactor(0);
+        this.deuxiemeplan.setOrigin(0,0);
+        this.deuxiemeplan.alpha=1;
+        //this.sky.tileScaleX=this.sky.tileScaleY=0.8;
 
-        this.sky3=this.add.tileSprite(
+        this.premierplan=this.add.tileSprite(
             0,
             0,
             this.sys.canvas.width,
             this.sys.canvas.height,
-            'sky-3'
+            'premier-plan'
             );
-        this.sky3.setScrollFactor(0);
-        this.sky3.setOrigin(0,0);
-        this.sky3.alpha=1;
+        this.premierplan.setScrollFactor(0);
+        this.premierplan.setOrigin(0,0);
+        this.premierplan.alpha=1;
 
         //fait passer les éléments devant le ciel
         this.platforms.setDepth(10)
@@ -94,14 +94,14 @@ class TableauScrolling extends Tableau{
     update(){
         super.update();
         //le ciel se déplace moins vite que la caméra pour donner un effet paralax
-        this.sky.tilePositionX=this.cameras.main.scrollX*0.6;
-        this.sky.tilePositionY=this.cameras.main.scrollY*0.2;
+        this.ciel.tilePositionX=this.cameras.main.scrollX*0.6;
+        this.ciel.tilePositionY=this.cameras.main.scrollY*0.2;
         //le deuxième ciel se déplace moins vite pour accentuer l'effet
-        this.sky2.tilePositionX=this.cameras.main.scrollX*0.3+500;
-        this.sky2.tilePositionY=this.cameras.main.scrollY*0.1+30;
+        this.premierplan.tilePositionX=this.cameras.main.scrollX*0.3+500;
+        this.premierplan.tilePositionY=this.cameras.main.scrollY*0.1+30;
 
-        this.sky3.tilePositionX=this.cameras.main.scrollX*0.5+500;
-        this.sky3.tilePositionY=this.cameras.main.scrollY*0.1+30;
+        this.deuxiemeplan.tilePositionX=this.cameras.main.scrollX*0.5+500;
+        this.deuxiemeplan.tilePositionY=this.cameras.main.scrollY*0.1+30;
 
     }
 }
